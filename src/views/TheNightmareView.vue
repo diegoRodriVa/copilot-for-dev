a6af0c26ad67ba22b036b538ee87e28101427afc
Diego Rodríguez <drvarela@plainconcepts.com>
2025-04-02 21:59:50 +0200

    feat: align horizontally and vertically

---
+++ src/views/TheNightmareView.vue
@@ -0,0 +1,45 @@
+<script setup lang="ts">
+
+const message = 'I am the nightmare of every backend developer! 😱'
+const anotherDiv = 'Another div 😢'
+const yetAnotherDiv = 'Yet another div 😭😭'
+
+</script>
+
+<template>
+  <div class="nightmare">
+    <h1>{{ message }}</h1>
+    <h2>{{ anotherDiv }} </h2>
+    <h2>{{ yetAnotherDiv }} </h2>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+$three-quarters: 75%;
+$half: 50%;
+$fourth: 25%;
+
+.nightmare {
+  height: 300px;
+  padding: 20px;
+
+  background-color: grey;
+
+  h1 {
+    color: #333;
+    font-size: 1.5rem;
+    background-color: red;
+    width: $three-quarters;
+  }
+
+  h2:nth-child(even) {
+    background-color: green;
+    width: $fourth;
+  }
+
+  h2:nth-child(odd) {
+    background-color: blue;
+    width: $half;
+  }
+}
+</style>
