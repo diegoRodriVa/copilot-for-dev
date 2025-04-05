7a5757ac3e2cd08398e01464134c09fe6e2fa821
Diego Rodríguez <drvarela@plainconcepts.com>
2025-04-05 18:12:31 +0200

    chore: improve comments for copilot function improvement

--- src/utils/whatAnUtils.ts
+++ src/utils/whatAnUtils.ts
@@ -1,10 +1,13 @@
 /* eslint-disable */
 /*
   What are we going to do here?
+  0. Provide context with this comments
   1. Understand the code
   2. Refactor it
   3. Type it
   4. Test it
+  5. Don't change the name of the function
+  6. Use more meaningful names for the variables and parameters
 */
 export const whatDoesItDo = (xs: any, key: any) => {
   return xs.reduce(function (rv: any, x: any) {
